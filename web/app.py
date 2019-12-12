b67eaf11c5394c9b0aa9fe950f01fd6b19a79a94
drewskiwooskie <drewskiwooskie@gmail.com>
2019-12-12 09:46:58 -0500

    forgot f

--- web/app.py
+++ web/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_whale():
     test = os.getenv("test")
-    return "{test} hello there!"
+    return f"{test} hello there!"
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
