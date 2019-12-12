084fffeb404cb0abdedc7ac7c65059185d9eaed8
drewskiwooskie <drewskiwooskie@gmail.com>
2019-12-12 09:45:05 -0500

    add test

--- web/app.py
+++ web/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
- 
+import os
 app = Flask(__name__)
  
  
 @app.route('/')
 def hello_whale():
-    return 'hello there!'
+    test = os.getenv("test")
+    return "{test} hello there!"
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
